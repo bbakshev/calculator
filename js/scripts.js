dcf0be1f58cbe85625a02c61dfae043ddf349059
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-09 14:01:52 -0800

    added all 4 alerts into one lalert

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 //business logic
-function add(number1,number2) {
-  return number1 + number2;
+function add(number1, number2) {
+  const result = number1 + number2
+  return "The addition of your numbers equals " + result;
 }
+
+function subtract(number1, number2) {
+  const result = number1 - number2
+  return "The subtraction of your numbers equals " + result;
+}
+
+function multiply(number1, number2) {
+  const result = number1 * number2
+  return "The multiplication of your numbers equals " + result;
+}
+
+function divide(number1, number2) {
+  const result = number1 / number2
+  return "The division of your numbers equals " + result;
+}
+
+
 //UI
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
-
+window.alert(add(number1, number2) + "\n" + subtract(number1, number2) + "\n" + multiply(number1, number2) + "\n" + divide(number1, number2));
