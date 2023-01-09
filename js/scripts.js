d73d687334ef3b7de4e8839597a2524d4df32c51
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-09 14:12:25 -0800

    added more functions of the calculator so the user can view what numbers they typed and the results

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
 //business logic
 function add(number1, number2) {
   const result = number1 + number2
-  return "The addition of your numbers equals " + result;
+  //return "The addition of your numbers equals " + result;
+  return number1 + "+" + number2 + "=" + result;
 }
 
 function subtract(number1, number2) {
   const result = number1 - number2
-  return "The subtraction of your numbers equals " + result;
+  //return "The subtraction of your numbers equals " + result;
+  return number1 + "-" + number2 + "=" + result;
 }
 
 function multiply(number1, number2) {
   const result = number1 * number2
-  return "The multiplication of your numbers equals " + result;
+  //return "The multiplication of your numbers equals " + result;
+  return number1 + "*" + number2 + "=" + result;
 }
 
 function divide(number1, number2) {
   const result = number1 / number2
-  return "The division of your numbers equals " + result;
+  //return "The division of your numbers equals " + result;
+  return number1 + "/" + number2 + "=" + result;
 }
 
 
@@ -24,4 +28,6 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2) + "\n" + subtract(number1, number2) + "\n" + multiply(number1, number2) + "\n" + divide(number1, number2));
+//window.alert(add(number1, number2) + "\n" + subtract(number1, number2) + "\n" + multiply(number1, number2) + "\n" + divide(number1, number2));
+
+window.alert(add(number1, number2) + ", " + subtract(number1, number2) + ", " + multiply(number1, number2) + ", " + divide(number1, number2));
