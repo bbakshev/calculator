fc33dae3c3a72f41cf2400b53413e93cf107967d
Brishna Bakshev <b.bakshev@gmail.com>
2023-01-09 13:35:28 -0800

    added more functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
+//business logic
 function add(number1,number2) {
   return number1 + number2;
 }
-const result = add(3,5);
-const outputText = "The sum is " + result + ".";
-saySomething(outputText);
+//UI
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
+
